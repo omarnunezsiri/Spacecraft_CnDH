d4f2e7e83846b5e585acd00f30a11689376061d0
Omar Nunez <omarnunezsiri@gmail.com>
2023-10-19 21:11:43 -0400

    Resolve merge conflicts

--- JAPI/Handlers/FileHandler.cs
+++ JAPI/Handlers/FileHandler.cs
@@ -11,35 +11,6 @@ public class FileHandler
     private FileHandler() { }
 
     public static FileHandler GetFileHandler() { return Instance; }
-  
-    /// <summary>
-    /// This method reads from the given file to read in all IP Addresses and Service IDs, puts them in a sictionary and returns them.
-    /// </summary>
-    /// <param name="fileName">File to read from</param>
-    public Dictionary<int, string> ReadIpConfigFile(string fileName)
-    {
-        Dictionary<int, string> serviceDictionary = new Dictionary<int, string>();
-        try
-        {
-            using (StreamReader sr = new StreamReader(fileName))
-            {
-                while (!sr.EndOfStream)
-                {
-                    string? line = sr.ReadLine();
-                    if (!String.IsNullOrEmpty(line))
-                    {
-                        line.Trim();
-                        string[] serviceInformation = line.Split(',');
-                        serviceDictionary.Add(int.Parse(serviceInformation[1]), serviceInformation[0]);
-                    }
-                }
-            }
-        }
-        catch (FileNotFoundException) { throw new FileNotFoundException($"Could not find the following file attempting to be opened: {fileName}"); }
-        catch (IOException) { throw new IOException($"There was an error trying to perform IO operations on the following file: {fileName}"); }
-        catch (Exception) { throw; }
-        return serviceDictionary;
-    }
 
     /// <summary>
     /// This method reads from the given file to read in all IP Addresses and Service IDs, puts them in a sictionary and returns them.
